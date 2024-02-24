b5153e90a4e01f89a806e1d86acb5261bdf8d5a0
Miyuru Gunatilake <rohangunatilake@gmail.com>
2024-02-24 16:35:05 +0530

    The Quiz Game

---
+++ game1.py
@@ -0,0 +1,15 @@
+print("Hi,Welcome")
+print("Yes or No Quiz")
+playing = input("Do You Want To Play? ")
+if playing != "Yes":
+    quit()
+print("Okay. Let's Play :)")
+print("I'm Fine/I'm Not Fine")
+answer = input("How Are You? ")
+if answer == "I'm Fine":
+    print("Good :))")
+else:
+    print("Oh Im Sorry :(")
+
+#this is my starter project
+#The Quiz Game
