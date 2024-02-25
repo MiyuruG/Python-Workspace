359ded4b2fc2dafb35b9467afe8a9d69851d2c89
Miyuru Gunatilake <rohangunatilake@gmail.com>
2024-02-25 14:04:39 +0530

    in line 27 is in an error.i don't know why is error in there.YT link 15.41 timeline in "https://www.youtube.com/watch?v=waY3LfJhQLY".I wrote until then.

--- pygame1.py
+++ pygame1.py
@@ -2,18 +2,35 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 500, 400
+WIDTH, HEIGHT = 750, 650
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Survive Time")
+pygame.display.set_caption("Survive From Trash")
+
+BG = pygame.image.load("trash.jpg")
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+
+def draw(player):
+    WIN.blit(BG, (0, 0))
+
+    player.draw.rect(WIN, "red", player)
+
+    pygame.display.update()
 
 def main():
     run = True
+
+player = pygame.rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(player)
 
     pygame.quit()
 
